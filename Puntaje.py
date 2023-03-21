1f04e1cdbbba286ec2c3035ec866a951500ca508
blackmyze <grsebas04@gmail.com>
2023-03-20 19:57:00 -0500

    adding the first part of check triki

--- Puntaje.py
+++ Puntaje.py
@@ -1,20 +1,66 @@
-from Siguiente_Movimiento import main
+from Siguiente_Movimiento import main, chars
 from Class.tree import Tree
+import numpy as np
 
-def score(tree, depth=1):
+# Funciones determinantes de triki
 
-    for i in main(tree, depth):
-        tree.branches.append(Tree(i))
+def on_row(row, depth):
+  """ Retorna False si char se encuentra en la lista(columna) es decir, si el jugador no ha hecho triki en la columna """
+  return False if ( chars[depth%2] in row or ' ' in row) else True
+  
 
-    for branch in tree.branches:
-        score(branch, depth+1)
+
+
+def score(tree, depth=0):
+    '''
+    Funcion principal del codigo
+    '''
+
+    result = 0
+
+    # -------- Comprobando triki -------------
+    for num_row,row in enumerate(tree.label):
+        
+        # ~~~~ Analizando por fila ~~~~
+        if ( on_row(row, depth+1) ): result = (-1)**(depth)
+        # ~~~~ Analizando por columna ~~~~
+        elif ( on_row(np.matrix(tree.label).transpose().tolist()[num_row], depth) ): 
+            result = (-1)**(depth)
+
+        # ~~~~ hacer los otros casos ~~~~
+
+
+
+    # --------- Buscar cada <Siguiente_Movimiento> a el nodo --------
+
+    for i in main(tree, depth):         # Recorrer cada 'Siguiente_Movimiento' de tree
+        tree.branches.append(Tree(i))   # Agregar como hijo a cada 'Siguiente_Movimiento'
+    
+    # -------- Recursion --------
+    for branch in tree.branches:        # Aplicar score a cada hijo de tree (recursion)
+        if (depth%2==0):
+            result = max(result,score(branch, depth+1))
+        else: 
+            result = min(result,score(branch, depth+1))
+
+    return result
+
+
+
+
+# Aca inicia la prueba (Codigo principal)
+
 
 def prueba():
+       
     t = Tree([["X","O","X"],
               ["O","X","O"],
-              [" "," "," "]])
+              ["X"," "," "]])
 
-    score(t)
+    x = score(t)
+    print(x)
     print(t)
 
 prueba()
+
+
--- Siguiente_Movimiento.py
+++ Siguiente_Movimiento.py
@@ -1,6 +1,7 @@
 from Class.queue import LinkedQueue
 
 auxqueue = LinkedQueue()
+chars = ['X','O'] # Opciones de marcado en el tablero, para el jugador A y B respectivamente
 
 def Siguiente_Movimiento(tree, depth):
   """ Calcula uno de los mejores movimientos siguientes
@@ -8,20 +9,19 @@ def Siguiente_Movimiento(tree, depth):
   >>> Siguiente_Movimiento(t)
   Tree([["X","O","X"],["O","X","O"],["O"," "," "]])
   """
-  chars = ['X','O']
   MainIndex = 0 # Recorre cada lista(fila) dentro de <tree.label>
-  label_copy = [tree.label[i].copy() for i in range(len(tree.label))]
+  label_copy = [tree.label[i].copy() for i in range(len(tree.label))]   # Copia de lo que almacena el arbol
 
-  for MainIndex in range(len(tree.label)):
+  for MainIndex in range(len(tree.label)):  # Recorrer cada fila del tablero de triki
 
     if (' ' in tree.label[MainIndex]): # Condicion donde ' ' esta dentro de la lista(fila) con indice 'MainIndex'
 
-      for SegIndex in range(len(tree.label)):
+      for SegIndex in range(len(tree.label)):   # Solo un for anidado
 
-        if (tree.label[MainIndex][SegIndex]== ' '):
-          label_copy[MainIndex][SegIndex] = chars[depth%2]
-          auxqueue.enqueue([i.copy() for i in label_copy])
-          label_copy[MainIndex][SegIndex] = ' '
+        if (tree.label[MainIndex][SegIndex]== ' '): # Verificar si la posicion del tablero esta vacia
+          label_copy[MainIndex][SegIndex] = chars[depth%2] # Cambiar la posicion vacia por la jugada correspondiente segun la profundidad
+          auxqueue.enqueue([i.copy() for i in label_copy]) # Agregar a la cola una copia del arbol siguiente
+          label_copy[MainIndex][SegIndex] = ' ' # Dejar como estaba la posicion original
 
 def main(t, depth):
     Siguiente_Movimiento(t, depth)
